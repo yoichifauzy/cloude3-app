191e94b48f5f64234ddaad8bbfd66d017b4e51c6
reyjinn-IT <raihanpermadi17@gmail.com>
2025-10-08 10:53:26 +0700

    update api 2

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 use App\Http\Controllers\Api\Auth\RegisterController;
 use App\Http\Controllers\Api\Auth\LoginController;
 use App\Http\Controllers\Api\UserController;
 
 Route::post('/auth/register', [RegisterController::class, 'register']);
-Route::post('/auth/login', [LoginController::class, 'login']);
+Route::post('/auth/login', LoginController::class);
 Route::get('/data', [UserController::class, 'index']);
+Route::middleware('auth:sanctum')->get('/user', function(Request $request) {
+    return $request->user();
+});
